d1d61bbac1683c6678f620f462307b06710cb4b4
shaunmicheal <shaunjeranyama@gmail.com>
2026-07-24 12:29:39 +0200

    adding responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,48 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const closeMenu = () => setIsOpen(false);
+
   return (
     <nav className="navigation">
       <div className="logo">Bean & Brew</div>
-      <ul className="links">
+
+      <ul className={`links ${isOpen ? "open" : ""}`}>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/" onClick={closeMenu}>
+            Home
+          </Link>
         </li>
         <li>
-          <Link to="/about">About</Link>
+          <Link to="/about" onClick={closeMenu}>
+            About
+          </Link>
         </li>
         <li>
-          <Link to="/contact">Contact</Link>
+          <Link to="/contact" onClick={closeMenu}>
+            Contact
+          </Link>
         </li>
         <li>
-          <Link to="/menu">Menu</Link>
+          <Link to="/menu" onClick={closeMenu}>
+            Menu
+          </Link>
         </li>
       </ul>
+
+      <button
+        className={`hamburger ${isOpen ? "open" : ""}`}
+        onClick={() => setIsOpen(!isOpen)}
+        aria-label="Toggle menu"
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
     </nav>
   );
 }
